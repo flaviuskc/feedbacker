deaf773e4e4d719b78d1eed40ad97d250a99d0ee
Antonio Costa <akcosta@pdmfc.com>
2023-09-05 18:07:20 +0100

    feat: faq banner

---
+++ dashboard/src/components/DoubtsBanner.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="flex flex-col items-center gap-9 text-gray-700 p-10 md:py-14">
+        <div class="flex flex-col items-center gap-2">
+            <h2 class="text-3xl font-black md:text-4xl lg:text-5xl">Any doubts?</h2>
+            <p class="text-lg text-center md:text-xl lg:text-2xl">Do you want to know how it works and what the prices are?</p>
+        </div>
+        <a href="mailto:flaviuskc@gmail.com">
+            <button class="rounded-full bg-pink-550 text-white font-bold py-2 px-6 text-base lg:text-lg">
+                Send us an email!
+            </button>
+        </a>
+    </div>
+  </template>
+  
+  <script setup lang="ts">
+  
+  </script>
