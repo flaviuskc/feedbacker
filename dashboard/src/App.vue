81235d63d39c1153698dc301048a0a53520f026b
Antonio Costa <akcosta@pdmfc.com>
2023-09-05 18:08:55 +0100

    app component

--- dashboard/src/App.vue
+++ dashboard/src/App.vue
@@ -1,85 +1,44 @@
-<script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <div>
+    <TheNavbar/>
+    <AppModal @changeUser="changeUser($event)"/>
+    <RouterView />
+    <TheFooter />
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
+<script setup lang="ts">
+  import { watch, ref } from 'vue'
+  import { RouterView, useRoute, useRouter } from 'vue-router'
+  // import './utils/axios'
+  import TheNavbar from '@/components/TheNavbar.vue'
+  import TheFooter from './components/TheFooter.vue'
+  import AppModal from './components/AppModal.vue'
+  import { useUserStore } from '@/stores/UserStore'
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
+  const route = useRoute()
+  const router = useRouter()
+  const userStore = useUserStore()
+  let updateUserLabelButton = ref(false)
 
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+  function changeUser(change: boolean) {
+    console.log('chegou aqui', change)
+    if(change){
+      updateUserLabelButton.value = true
+    }
+    console.log('atualiza: ', updateUserLabelButton.value)
   }
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
+  watch(() => route.path, () => {
+  if(route.meta.hasAuth){
+    const token = userStore.userLogin.token
+    if(!token){
+      router.push({ name: 'Home' })
+      return
+    }
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
+    const user = userStore.getUserLogged
+    // userStore.userLogin.
   }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
+})
+</script>
