f51a710c7e063bee58fbd080ea338c7ffcea86fc
Antonio Costa <akcosta@pdmfc.com>
2023-09-05 18:10:51 +0100

    feat: feedback list component

---
+++ dashboard/src/components/FeedbackList.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="flex flex-col gap-6 items-start justify-start">
+        <div class="flex flex-col gap-4 bg-gray-50 p-6 rounded w-full">
+            <div class="flex flex-row justify-between">
+                <span>tag</span>
+                <span>tempo</span>
+            </div>
+            <p>description</p>
+            <ph-caret-down
+                class="self-end cursor-pointer"
+                :size="24"
+            />
+        </div>
+        <div class="flex flex-col gap-4 bg-gray-50 p-6 rounded w-full">
+            <div class="flex flex-row justify-between">
+                <span>tag</span>
+                <span>tempo</span>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+    import { PhCaretDown } from '@phosphor-icons/vue'
+</script>
