f49697d2a5c8a666470e0a76cc7b240b5cd8ea42
Antonio Costa <akcosta@pdmfc.com>
2023-09-05 18:10:31 +0100

    feat: app header

---
+++ dashboard/src/components/AppInnerHeader.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="bg-gray-100 flex flex-col items-center gap-2 p-8 md:p-16 lg:p-24">
+        <h1 class="font-black text-3xl md:text-4xl lg:text-5xl">{{ title }}</h1>
+        <h2 class="text-lg text-center md:text-xl lg:text-2xl">{{ subtitle }}</h2>
+    </div>
+</template>
+
+<script setup lang="ts">
+    defineProps<{
+        title: String,
+        subtitle?: String
+    }>()
+</script>
