628ee5c43ee4d546c50f49fa2ae44833c99a9d6a
Antonio Costa <akcosta@pdmfc.com>
2023-09-05 18:10:13 +0100

    feat: filter component

---
+++ dashboard/src/components/AppLateralFilter.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h3 class="text-lg mb-4">Filters</h3>
+        <div class="flex flex-col gap-2 text-sm">
+            <div v-for="(filter,index) in filters" :key="index">
+                <AppLateralFilterItem :filter="filter"/>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+    import AppLateralFilterItem from './AppLateralFilterItem.vue'
+
+    defineProps<{
+        filters: any[]
+    }>()
+</script>
---
+++ dashboard/src/components/AppLateralFilterItem.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="bg-transparent flex flex-row justify-between items-center rounded py-1 px-2 cursor-pointer hover:bg-gray-100">
+        <div class="flex flex-row gap-2 items-center">
+            <div class="rounded-full bg-blue-600 h-1 w-1"></div>
+            <span>{{ filter }}</span>
+        </div>
+        <span>300</span>
+    </div>
+</template>
+<script setup lang="ts">
+    defineProps<{
+        filter: object
+    }>()
+</script>
