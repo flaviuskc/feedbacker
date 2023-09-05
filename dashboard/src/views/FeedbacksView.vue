7cfa85cf90e7f8ecf61739ccf292bcecd3033db0
Antonio Costa <akcosta@pdmfc.com>
2023-09-05 18:09:47 +0100

    feat: feedback page

---
+++ dashboard/src/views/FeedbacksView.vue
@@ -0,0 +1,28 @@
+<template>
+    <AppInnerHeaderVue 
+        :title="'Feedbacks'"
+        :subtitle="'Details of all feedbacks received'"
+    />
+    <div class="flex justify-center py-6">
+        <div class="flex flex-col justify-between gap-8 w-4/5 md:flex-row md:gap-16">
+            <AppLateralFilter class="md:w-1/4" :filters="filters"/>
+            <FeedbackList class="md:w-3/4" />
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+    import AppInnerHeaderVue from '@/components/AppInnerHeader.vue'
+    import AppLateralFilter from '@/components/AppLateralFilter.vue'
+    import FeedbackList from '@/components/FeedbackList.vue'
+    import { onMounted } from 'vue'
+    import { useUserStore } from '@/stores/UserStore'
+    import { useFeedbackStore } from '@/stores/FeedbackStore'
+
+    const feedbackStore = useFeedbackStore()
+    let filters = [{filter:'oi'}, {filter:'tcha'}]
+
+    onMounted(() => {
+        feedbackStore.getFilters()
+    })
+</script>
